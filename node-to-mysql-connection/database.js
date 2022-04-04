b05b649bf230b5180c7d2bd67c869404ef06000c
Aditya Neve <aditya.neve@gmail.com>
2022-04-04 12:12:49 +0530

    createConnection

--- node-to-mysql-connection/database.js
+++ node-to-mysql-connection/database.js
@@ -1,4 +1,4 @@
-const { createPool } = require('mysql');
+/* const { createPool } = require('mysql');
 
 console.clear();
 
@@ -15,4 +15,36 @@ pool.query(`SELECT * FROM sql_store.customers`, (error, response) => {
 let sqlQuery = `CALL sql_store.get_all_customers_name()`
 pool.query(sqlQuery, [], (error, response) => {
     console.log("🚀 ~ file: database.js ~ line 14 ~ pool.query ~ response", response)
-})
+}) */
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'todosapp'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+
+    let createTodos = `CREATE TABLE IF NOT EXISTS todo(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(300) NOT NULL,
+    status tinyint(1) NOT NULL DEFAULT 0
+    );
+    `;
+
+
+    connection.query(createTodos, function (error, results, fields) {
+        if (error) throw error;
+        // connected!
+        console.log(results)
+    });
+
+});
